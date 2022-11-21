69d39c529162e65bbeb05ff712e436e497cdf149
fatihrizqon <fatihrizqon@gmail.com>
2022-11-21 16:35:41 +0700

    Initial Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,28 +1,49 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="author" content="Laravel">
-        <meta name="description"
-            content="Laravel is a web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+        <meta name="author" content="Laravel" />
+        <meta
+            name="description"
+            content="Laravel is a web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things."
+        />
         <link rel="canonical" href="{{ env('APP_URL') }}" />
-        <title>{{ env('APP_NAME', 'Laravel') }}</title>
+        <title>{{ env("APP_NAME", "Laravel") }}</title>
 
         <!-- AdminLTE -->
         <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
             integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-            crossorigin="anonymous" referrerpolicy="no-referrer" />
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
         <!-- iCheck -->
-        <link rel="stylesheet" href="{{ asset('admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+        <link
+            rel="stylesheet"
+            href="{{
+                asset(
+                    'admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css'
+                )
+            }}"
+        />
         <!-- Theme style -->
-        <link rel="stylesheet" href="{{ asset('admin-lte/dist/css/adminlte.min.css') }}">
+        <link
+            rel="stylesheet"
+            href="{{ asset('admin-lte/dist/css/adminlte.min.css') }}"
+        />
         <!-- overlayScrollbars -->
-        <link rel="stylesheet" href="{{ asset('admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
-
+        <link
+            rel="stylesheet"
+            href="{{
+                asset(
+                    'admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css'
+                )
+            }}"
+        />
         <!-- Scripts -->
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
@@ -31,11 +52,18 @@
         <div class="wrapper">
             <header>
                 <!-- Navbar -->
-                <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+                <nav
+                    class="main-header navbar navbar-expand navbar-white navbar-light"
+                >
                     <!-- Left -->
                     <ul class="navbar-nav ml-4">
                         <li class="nav-item">
-                            <a class="nav-link" data-widget="pushmenu" href="#" role="button">
+                            <a
+                                class="nav-link"
+                                data-widget="pushmenu"
+                                href="#"
+                                role="button"
+                            >
                                 <i class="fa-solid fa-bars"></i>
                             </a>
                         </li>
@@ -46,17 +74,28 @@
                         <!-- Notifications Dropdown Menu -->
                         <li class="nav-item dropdown">
                             <a class="nav-link" data-toggle="dropdown" href="#">
-                                <i class="far fa-user"></i> {{ Auth::user()->name }}
+                                <i class="far fa-user"></i>
+                                {{ Auth::user()->name }}
                             </a>
-                            <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                                <li class="dropdown-item dropdown-header">Account Settings</li>
+                            <ul
+                                class="dropdown-menu dropdown-menu-lg dropdown-menu-right"
+                            >
+                                <li class="dropdown-item dropdown-header">
+                                    Account Settings
+                                </li>
                                 <div class="dropdown-divider"></div>
                                 <li class="dropdown-item">
-                                    <form method="POST" action="{{ route('logout') }}">
+                                    <form
+                                        method="POST"
+                                        action="{{ route('logout') }}"
+                                    >
                                         @csrf
 
-                                        <a class="fs-sm text-danger text-decoration-none" href="route('logout')"
-                                            onclick="event.preventDefault(); this.closest('form').submit();">
+                                        <a
+                                            class="fs-sm text-danger text-decoration-none"
+                                            href="route('logout')"
+                                            onclick="event.preventDefault(); this.closest('form').submit();"
+                                        >
                                             Logout
                                         </a>
                                     </form>
@@ -64,7 +103,12 @@
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                            <a
+                                class="nav-link"
+                                data-widget="fullscreen"
+                                href="#"
+                                role="button"
+                            >
                                 <i class="fa-solid fa-expand-arrows-alt"></i>
                             </a>
                         </li>
@@ -73,41 +117,73 @@
             </header>
 
             <main>
-                <aside class="main-sidebar sidebar-dark-primary elevation-4">
+                <aside class="main-sidebar sidebar-dark-primary elevation-2">
                     <!-- Brand Logo -->
-                    <a href="{{ route('/') }}" class="brand-link text-decoration-none">
-                        <img src="https://laravel.com/img/logomark.min.svg" alt="{{ env('APP_NAME') }} Logo"
-                            class="brand-image" style="opacity: .8">
-                        <span class="brand-text font-weight-light">{{ env('APP_NAME') }}</span>
+                    <a
+                        href="{{ route('/') }}"
+                        class="brand-link text-decoration-none bg-white"
+                    >
+                        <img
+                            src="{{ asset('img/icon.png') }}"
+                            alt="{{ env('APP_NAME') }} Logo"
+                            class="brand-image"
+                        />
+                        <span class="brand-text fw-bold fs-xs"
+                            >Arsip Nasional Republik Indonesia</span
+                        >
                     </a>
 
                     <!-- Sidebar -->
                     <div class="sidebar">
                         <!-- Sidenav -->
                         <nav class="mt-2">
-                            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                                data-accordion="false">
+                            <ul
+                                class="nav nav-pills nav-sidebar flex-column"
+                                data-widget="treeview"
+                                role="menu"
+                                data-accordion="false"
+                            >
                                 @can('admin')
                                 <li class="nav-header">Admin Panel</li>
                                 <li class="nav-item">
-                                    <a href="{{ route('dashboard') }}" class="nav-link">
-                                        <i class="nav-icon fa-solid fa-tachometer-alt"></i>
+                                    <a
+                                        href="{{ route('dashboard') }}"
+                                        class="nav-link"
+                                    >
+                                        <i
+                                            class="nav-icon fa-solid fa-tachometer-alt"
+                                        ></i>
                                         <p>Dashboard</p>
                                     </a>
                                 </li>
                                 @endcan
                                 <li class="nav-item">
-                                    <a href="{{ route('dashboard.account_settings') }}" class="nav-link">
-                                        <i class="nav-icon fa-solid fa-screwdriver-wrench"></i>
+                                    <a
+                                        href="{{
+                                            route('dashboard.account_settings')
+                                        }}"
+                                        class="nav-link"
+                                    >
+                                        <i
+                                            class="nav-icon fa-solid fa-screwdriver-wrench"
+                                        ></i>
                                         <p>Account Settings</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <form method="POST" action="{{ route('logout') }}">
+                                    <form
+                                        method="POST"
+                                        action="{{ route('logout') }}"
+                                    >
                                         @csrf
-                                        <a type="submit" class="nav-link text-danger"
-                                            onclick="event.preventDefault(); this.closest('form').submit();">
-                                            <i class="nav-icon fa-solid fa-right-from-bracket"></i>
+                                        <a
+                                            type="submit"
+                                            class="nav-link text-danger"
+                                            onclick="event.preventDefault(); this.closest('form').submit();"
+                                        >
+                                            <i
+                                                class="nav-icon fa-solid fa-right-from-bracket"
+                                            ></i>
                                             <p>Logout</p>
                                         </a>
                                     </form>
@@ -126,8 +202,11 @@
                                     <!-- Breadcumbs -->
                                     <ol class="breadcrumb float-sm-left">
                                         <li class="breadcrumb-item active">
-                                            <a class="text-decoration-none tw-text-gray-600"
-                                                href="{{ route('dashboard') }}">Dashboard</a>
+                                            <a
+                                                class="text-decoration-none tw-text-gray-600"
+                                                href="{{ route('dashboard') }}"
+                                                >Dashboard</a
+                                            >
                                         </li>
                                     </ol>
                                 </div>
@@ -144,25 +223,32 @@
                 </div>
             </main>
 
-            <footer class="main-footer">
-                Admin LTE 3.1
-            </footer>
+            <footer class="main-footer">Admin LTE 3.1</footer>
         </div>
 
         <!-- jQuery -->
-        <script src="{{ asset('admin-lte/plugins/jquery/jquery.min.js') }}"></script>
+        <script src="{{
+                asset('admin-lte/plugins/jquery/jquery.min.js')
+            }}"></script>
         <!-- jQuery UI 1.11.4 -->
-        <script src="{{ asset('admin-lte/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+        <script src="{{
+                asset('admin-lte/plugins/jquery-ui/jquery-ui.min.js')
+            }}"></script>
         <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
         <script>
-            $.widget.bridge('uibutton', $.ui.button)
+            $.widget.bridge("uibutton", $.ui.button);
         </script>
         <!-- Bootstrap 4 -->
-        <script src="{{ asset('admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{
+                asset('admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js')
+            }}"></script>
         <!-- overlayScrollbars -->
-        <script src="{{ asset('admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+        <script src="{{
+                asset(
+                    'admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js'
+                )
+            }}"></script>
         <!-- AdminLTE App -->
         <script src="{{ asset('admin-lte/dist/js/adminlte.min.js') }}"></script>
     </body>
-
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('portals.index');
 })->name('/'); 
 
 Route::group(['prefix' => 'dashboard',  'middleware' => ['auth', 'verified']], function()
